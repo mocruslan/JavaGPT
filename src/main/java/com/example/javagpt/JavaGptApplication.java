fb1d1b11eac82b10a8065fb4c5668ef86623b189
ruslan <mochulskyy.ruslan@gmail.com>
2023-11-21 23:22:28 +0100

    Init

---
+++ src/main/java/com/example/javagpt/JavaGptApplication.java
@@ -0,0 +1,13 @@
+package com.example.javagpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javagpt/JavaGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javagpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
